b5d6b65acff228d4238595efc70681224264bcf3
Claas Abert <claas@cabird.de>
2012-06-05 01:25:48 +0200

    fix url matching for events

--- app/assets/javascripts/ajaxr.js
+++ app/assets/javascripts/ajaxr.js
@@ -20,9 +20,9 @@
 
   $.ajaxr = ajaxr = {
     // current absolute url
-    currentUrl: $(window.location).attr('href'),
+    nextUrl: $(window.location).attr('href'),
 
-    lastUrl: undefined,
+    currentUrl: undefined,
 
     // returns the json URL for a href. may be overridden
     jsonUrl: function(url) {
@@ -31,15 +31,14 @@
     },
     
     // find the first handler matching the urls and calls it
-    trigger: function(url, eventType, data) {
+    trigger: function(eventType, data) {
       var i = handlers[eventType].length;
       while(i--) {
         var item = handlers[eventType][i];
-        var srcPattern    = item[0],
+        var sourcePattern = item[0],
             targetPattern = item[1],
             handler       = item[2];
-        if (ajaxr.currentUrl.match(srcPattern) && url.match(targetPattern)) {
-          data.push(url);
+        if (ajaxr.currentUrl.match(sourcePattern) && ajaxr.nextUrl.match(targetPattern)) {
           return handler.apply(this, data);
         }
       }
@@ -54,17 +53,17 @@
     //registerEvent: function(eventType, urlPattern, handler) {
     registerEvent: function(eventType) {
       // TODO error checking, parameter types...
-      var srcPattern, targetPattern, handler;
+      var sourcePattern, targetPattern, handler;
       if (arguments.length == 3) {
-        srcPattern    = /.*/;
+        sourcePattern = /.*/;
         targetPattern = arguments[1];
         handler       = arguments[2];
       } else if (arguments.length == 4) {
-        srcPattern    = arguments[1];
+        sourcePattern = arguments[1];
         targetPattern = arguments[2];
         handler       = arguments[3];
       }
-      handlers[eventType].push([srcPattern, targetPattern, handler]);
+      handlers[eventType].push([sourcePattern, targetPattern, handler]);
     },
 
     // handles the AJAX request
@@ -72,13 +71,13 @@
       var options = $.extend(ajaxr.optionsForUrl(url), {
         url: ajaxr.jsonUrl(url),
         beforeSend: function(xhr, settings) {
-          return ajaxr.trigger(url, 'beforeSend', [xhr, settings]);
+          return ajaxr.trigger('beforeSend', [xhr, settings]);
         },
         success: function(data, status, xhr) {
-          ajaxr.trigger(url, 'success', [data, status, xhr]);
+          ajaxr.trigger('success', [data, status, xhr]);
         },
         complete: function(xhr, status) {
-          ajaxr.trigger(url, 'complete', [xhr, status]);
+          ajaxr.trigger('complete', [xhr, status]);
         },
         error: function(xhr, status, error) {
           ajaxr.trigger('error', [xhr, status, error]);
@@ -90,7 +89,7 @@
   };
   
   // register default success handler
-  ajaxr.registerEvent('success', /.*/, function(data, status, xhr, url) {
+  ajaxr.registerEvent('success', /.*/, function(data, status, xhr) {
     $.each(data, function(key, value) {
       $("#" + key).html(value);
     });
@@ -106,9 +105,9 @@
     scroll = 0;
 
     var State = History.getState();
+    $.ajaxr.currentUrl = $.ajaxr.nextUrl;
+    $.ajaxr.nextUrl = State.url;
     ajaxr.handleRequest(State.url);
-    $.ajaxr.lastUrl    = $.ajaxr.currentUrl;
-    $.ajaxr.currentUrl = State.url;
   });
 
   // register links with remote and history enabled
